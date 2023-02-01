38149359844dc68d399bf8dbbdcac2c749840502
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-01 00:29:40 -0300

    função das barreiras

--- js/flappy.js
+++ js/flappy.js
@@ -20,9 +20,35 @@ function Barreira(reversa = false){
 //b.setAltura(200)
 //document.querySelector('[tp-flappy]').appendChild(b.elemento)
 
-function ParDeBarreira(altura, abertura, x){
+function ParDeBarreiras(altura, abertura, x){
     this.elemento = novoElemento('div', 'par-de-barreira')
 
     this.superior = new Barreira(true)
     this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        
+        const alturaInferior = altura - abertura - alturaSuperior
+
+        this.superior.setAltura(alturaSuperior)
+
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+
+    this.setX = x => this.elemento.style.left = `${x}px`
+    
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+
 }
+
+const b = new ParDeBarreiras(700,200,800)
+document.querySelector('[tp-flappy]').appendChild(b.elemento)
