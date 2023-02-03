e059591fc632892bc251c8558571c5f2939d4b31
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-02 23:19:01 -0300

    criando função dos obstáculos em movimento

--- js/flappy.js
+++ js/flappy.js
@@ -21,7 +21,7 @@ function Barreira(reversa = false){
 //document.querySelector('[tp-flappy]').appendChild(b.elemento)
 
 function ParDeBarreiras(altura, abertura, x){
-    this.elemento = novoElemento('div', 'par-de-barreira')
+    this.elemento = novoElemento('div', 'par-de-barreiras')
 
     this.superior = new Barreira(true)
     this.inferior = new Barreira(false)
@@ -50,5 +50,41 @@ function ParDeBarreiras(altura, abertura, x){
 
 }
 
-const b = new ParDeBarreiras(700,200,800)
-document.querySelector('[tp-flappy]').appendChild(b.elemento)
+//const b = new ParDeBarreiras(700,300,800)
+//document.querySelector('[tp-flappy]').appendChild(b.elemento)
+
+function Barreiras (altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura ),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3),
+    ]
+
+    const deslocamento = 3
+
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento)
+       
+            //barreiras saíram da tela, acontece isso
+            if(par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbertura()
+            }
+            const meio = largura / 2
+            const cruzouOMeio = par.getX() + deslocamento >= meio && par.getX() < meio
+                if(cruzouOMeio) notificarPonto()
+       
+        })
+    }
+}
+
+const barreiras = new Barreiras(700, 1200, 300, 400)
+
+const areaDoJogo = document.querySelector('[tp-flappy]')
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+setInterval( () =>{
+    barreiras.animar()
+}, 20)
